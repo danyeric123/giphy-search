09b7e1b40db1560a0665f0543dc25c54b31751fe
danyeric123 <dnagarpowers@gmail.com>
2021-08-04 18:52:36 -0400

    cleaned up the code

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css';
+import React, { useState } from 'react';
+import Giphy from './component/Giphy';
+
+
+const App = ()=>{
+  const [giphys, setGiphy] = useState([])
+  const [searchInput, setSearchInput] = useState("")
+  
+  let handleChange = (event)=> {
+    setSearchInput({ [event.target.name]: event.target.value })
+  }
+  let handleSubmit = (event)=>{
+    event.preventDefault()
+
+    const url = `http://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${searchInput.searchInput}`
+    fetch(url)
+        .then(response=> response.json())
+      .then(data=> {
+        setGiphy([...data.data])
+      })
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='searchInput'>Search</label>
+        <input
+          name='searchInput'
+          type='text'
+          value={searchInput.value}
+          onChange={handleChange}
+        />
+        <button type='submit'>Search</button>
+      </form>
+      {giphys.map(giphy=><Giphy key={giphy.images.original.url} giphy={giphy.images.original.url} />)}
+    </>
+  )
+}
+
+export default App;
---
+++ src/component/Giphy.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Giphy = ({giphy})=>{
+  return (
+    <>
+      <img 
+        src={giphy}
+        alt="result"
+      />
+    </>
+  )
+}
+
+export default Giphy
